948372f5a83a6dbffc06c7b461b54f527c780541
ggc1412 <ggc1412@gmail.com>
2020-03-26 22:29:11 +0900

    :rocket:Building for production

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import morgan from "morgan";
 import helmet from "helmet";
 import session from "express-session";
+import path from "path";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import passport from "passport";
@@ -26,8 +27,8 @@ const CookieStore = MongoStore(session);
 
 app.use(helmet());
 app.set("view engine", "pug");
-//app.use("/uploads", express.static("uploads"));
-app.use("/static", express.static("static"));
+app.set("views", path.join(__dirname, "views"));
+app.use("/static", express.static(path.join(__dirname, "static")));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
