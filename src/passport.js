0112c770b38d607466f144ecf7cafc5f1076688a
ggc1412 <ggc1412@gmail.com>
2020-03-27 16:15:18 +0900

    :bug:oauth callback bug fix

--- src/passport.js
+++ src/passport.js
@@ -13,10 +13,9 @@ passport.use(
     {
       clientID: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      callbackURL: `https://serene-dusk-80611.herokuapp.com${routes.githubCallback}`
-      // process.env.PRODUCTION
-      //   ? `https://serene-dusk-80611.herokuapp.com${routes.githubCallback}`
-      //   : `http://localhost:3000${routes.githubCallback}`
+      callbackURL: process.env.PRODUCTION
+        ? `https://serene-dusk-80611.herokuapp.com${routes.githubCallback}`
+        : `http://localhost:3000${routes.githubCallback}`
     },
     githubLoginCallback
   )
